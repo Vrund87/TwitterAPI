839db0ae18955e07ab680be82d2675a812aab5e1
Vrund87 <vrundrajput2002@gmail.com>
2023-06-04 21:28:14 +0530

    Updated both the repo

--- src/main/java/com/v1/TwitterWebAPI/repositories/ElasticsearchUserRepo.java
+++ src/main/java/com/v1/TwitterWebAPI/repositories/ElasticsearchUserRepo.java
@@ -2,6 +2,7 @@ package com.v1.TwitterWebAPI.repositories;
 
 import com.v1.TwitterWebAPI.models.ElasticsearchTwitterUser;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface ElasticsearchUserRepo extends ElasticsearchRepository<ElasticsearchTwitterUser, String> {
 }
